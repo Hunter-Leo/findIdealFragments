a9f1590a0d77c21cdcbdc988a75294a30d392918
FightingLee97 <472279601@qq.com>
2025-06-05 15:42:16 +0800

    update test

--- test/test_find_next.py
+++ test/test_find_next.py
@@ -20,7 +20,7 @@ def test_first_run():
 
 def test_multi_run():
     # 测试大数组
-    arr = np.array([1,1,0,0,0,1,1, 0,0,0,0, 1,1,0,0,1,1,0,0,1,1])
+    arr = np.array([1,1,0,0,0,1,1, 0,0,0,0, 1,1,0,0,1,1,0,0,1,1, 0,0,0,1,0,0])
     first_rotator = IterableSequenceNumRotateCalculation(4, arr)
     first_run_score, first_run_windows = first_rotator.find_next_ideal_windows(1, "mean", True)
     print(f'{arr=}')
@@ -34,11 +34,12 @@ def test_multi_run():
     third_run_score, third_run_windows = third_rotator.find_next_ideal_windows(1, "mean", True)
     print(f'{third_run_score=}, {third_run_windows=}')
 
-    '''output:
-    arr=array([1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1])
-    first_run_score=0.5, first_run_windows=[(0, 1), (9, 9)]
-    second_run_score=0.5, second_run_windows=[(4, 2)]
-    third_run_score=None, third_run_windows=[]  # 此时整个队列已经分割完成，共三个连续窗口，每个窗口的分值都是 0.5。
+    '''output: 
+    arr=array([1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0,
+       0, 0, 1, 0, 0])
+    first_run_score=0.5, first_run_windows=[(0, 1), (4, 2), (9, 11)] # 第一轮共找到三个连续窗口， 长度分别为 1,2,11，每个窗口的分值都是 0.5。
+    second_run_score=0.25, second_run_windows=[(23, 1)] # 第二轮找到 1个连续窗口，每个窗口的分值都是 0.25。
+    third_run_score=None, third_run_windows=[] # 此时整个队列已经分割完成
     '''
     pass
 
